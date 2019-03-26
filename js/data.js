1a3b368f427e480db51a72a9d91856b7986c0912
Ines Akrap <ines.akrap@netcentric.biz>
2019-03-26 18:56:58 +0100

    HTML base, mock data, initial rendering and tab functionality

---
+++ js/data.js
@@ -0,0 +1,103 @@
+export const profiles = {
+  "profiles": [
+    {
+      "name": "Netcentric",
+      "userName": "netcentrichq",
+      "profilePictureUrl": "https://avatars3.githubusercontent.com/u/10152051?s=280&v=4",
+      "description": "Lorem Ipsum ... ",
+      "profileType": "Business",
+      "profileUrl": "https://www.netcentric.biz",
+      "followers": "384",
+      "following": "320",
+      "images": [
+        {
+          "imageUrl": "https://scontent.fham3-1.fna.fbcdn.net/v/t1.0-9/36525229_1752664994825267_23304398058815488_o.jpg?_nc_cat=101&_nc_ht=scontent.fham3-1.fna&oh=088a574e8dd24a5b1d9c7dd6dfe0f7ca&oe=5D05F156",
+          "liked": ["ines.nc", "samircro", "lavilla"],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        },
+        {
+          "imageUrl": "https://scontent.fham3-1.fna.fbcdn.net/v/t1.0-9/43878806_1898749326883499_6428251533011320832_o.jpg?_nc_cat=105&_nc_ht=scontent.fham3-1.fna&oh=bacbcd4d2a91eda84509ca84e37b260d&oe=5D469BDC",
+          "liked": [],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        },
+        {
+          "imageUrl": "https://scontent.fham3-1.fna.fbcdn.net/v/t1.0-9/38064079_1798851980206568_2755084115790266368_o.jpg?_nc_cat=105&_nc_ht=scontent.fham3-1.fna&oh=7d3b411c551441a414d6eb046dfd8c19&oe=5D4DF07D",
+          "liked": [],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        }
+      ]
+    },
+    {
+      "name": "Ines",
+      "userName": "ines.nc",
+      "profilePictureUrl": "https://avatars3.githubusercontent.com/u/10152051?s=280&v=4",
+      "description": "Lorem Ipsum ... ",
+      "profileType": "Personal Blog",
+      "profileUrl": "https://www.ines-akrap.com",
+      "followers": "384",
+      "following": "320",
+      "images": [
+        {
+          "imageUrl": "http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png",
+          "liked": [],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        },
+        {
+          "imageUrl": "http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png",
+          "liked": [],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        },
+        {
+          "imageUrl": "http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png",
+          "liked": [],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        }
+      ]
+    },
+    {
+      "name": "Claire",
+      "userName": "photoclaire",
+      "profilePictureUrl": "https://avatars3.githubusercontent.com/u/10152051?s=280&v=4",
+      "description": "Lorem Ipsum ... ",
+      "profileType": "Photography",
+      "profileUrl": "https://www.claire-photography.com",
+      "followers": "384",
+      "following": "320",
+      "images": [
+        {
+          "imageUrl": "http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png",
+          "liked": [],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        },
+        {
+          "imageUrl": "http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png",
+          "liked": [],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        },
+        {
+          "imageUrl": "http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png",
+          "liked": [],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        }
+      ]
+    }
+  ]
+}
---
+++ js/scripts.js
@@ -0,0 +1,101 @@
+import {profiles} from './data.js';
+
+const profileBox = document.querySelector('.profile');
+const tabPosts = document.querySelector('[data-selector-tab-posts]');
+const tabUsers = document.querySelector('[data-selector-tab-users]');
+const tabBar = document.querySelector('[data-active-tab]');
+const tabLinks = document.querySelectorAll('[data-link-tab]');
+
+const renderImages = (images) => {
+  images.forEach((image) => {
+    const post = document.createElement('div');
+    post.classList.add('post');
+    
+    const markupPost = `
+      <img class="post-image" src=${image.imageUrl} alt="Profile picture"></img>
+      <div class="post-content">
+          <div class="post-likes">
+            <button class="post-likes-icon">$</button>
+            <span class="post-likes-number">5</span>
+            <span class="post-likes-list">${image.liked}</span>
+          </div>
+          <div class="post-time">${image.timestamp}</div>
+      </div>
+    `;
+
+    post.innerHTML = markupPost;
+    tabPosts.appendChild(post);
+  });
+}
+
+const renderProfileBox = (profile) => {
+  const markupProfile = `
+    <img class="profile-image" src=${profile.profilePictureUrl} alt="Profile picture"></img>
+    <div class="profile-wrapper">
+        <h2 class="profile-name">${profile.name}</h2>
+        <p class="profile-user">${profile.userName}</p>
+        <p class="profile-bio">${profile.description}</p>
+        <a class="profile-link">${profile.profileUrl}</a>
+    </div>
+  `;
+  
+  profileBox.innerHTML = markupProfile;
+}
+
+const renderProfile = (profile) => {
+  renderProfileBox(profile);
+  renderImages(profile.images);
+}
+
+const renderUsers = (profiles) => {
+  const usersList = document.createElement('ul');
+  usersList.classList.add('usersList');
+  
+  profiles.forEach((profile) => {
+    const userWrapper = document.createElement('li');
+    userWrapper.classList.add('user');
+    userWrapper.setAttribute('data-user', profile.userName);
+    
+    const markupUser = `
+        <img class="user-image" src=${profile.profilePictureUrl} alt="Profile picture"></img>
+        <div class="user-data">
+          <div class="user-name">${profile.name}</div>
+          <div class="user-username">${profile.userName}</div>
+        </div>
+    `;
+    
+    userWrapper.innerHTML = markupUser;
+    usersList.appendChild(userWrapper);
+  });
+
+  tabUsers.appendChild(usersList);
+}
+
+const switchTab = (e) => {
+  const clickedLink = e.target.getAttribute('data-link-tab');
+  const activeTab = tabBar.getAttribute('data-active-tab');
+  if (clickedLink === activeTab) {return;}
+
+  tabBar.setAttribute('data-active-tab', clickedLink);
+  if (clickedLink === 'users' || activeTab === 'users') {
+    tabPosts.classList.toggle('active');
+    tabUsers.classList.toggle('active');
+    return;
+  }
+
+  tabPosts.classList.toggle('grid');
+  tabPosts.classList.toggle('list');
+}
+
+const initTabs = () => {
+  tabLinks.forEach((link) => link.addEventListener('click', switchTab));
+}
+
+const init = () => {
+  const initProfile = profiles.profiles[0];
+  renderProfile(initProfile);
+  renderUsers(profiles.profiles);
+  initTabs();
+}
+
+init();
