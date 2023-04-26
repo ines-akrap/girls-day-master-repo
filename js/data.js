1ba4840e509dce8daf22b89c7fa88056c24dc806
Ines Akrap <akrap.ines@gmail.com>
2023-04-26 21:36:18 +0200

    Data update

--- js/data.js
+++ js/data.js
@@ -11,35 +11,35 @@ export const profiles = {
       "following": "320",
       "images": [
         {
-          "imageUrl": "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+          "imageUrl": "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
           "liked": ["ines.nc", " samircro", " lavilla"],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "24-04-2023",
           "comments": []
         },
         {
-          "imageUrl": "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+          "imageUrl": "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "10-04-2023",
           "comments": []
         },
         {
-          "imageUrl": "https://images.unsplash.com/photo-1552664730-d307ca884978?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+          "imageUrl": "https://images.unsplash.com/photo-1552664730-d307ca884978?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
           "liked": ["photoclaire"],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "03-04-2023",
           "comments": []
         },
         {
-          "imageUrl": "https://lh3.googleusercontent.com/JZ_RTqqNIQx58nnVuBfsS-mufAlTkN1igG9UaM6GMWUJTA57xCH3-Av-b5JIWJhMtatKbwxBEMH1dz4ycJNoMtnGhp6KZf4-0ODpqpj7ARgRP-gmOGuE8lIQBxxGjbZE0A-th_LeZRnVIP05bz9-GpftaPH_y52HlPuhfk6_bnFK1bhH5h9bTw5fJjPrBEJNxt_w6vCujDN8xOB5mRz295NNJvCC-NVBJIIxpGSf-bMnxPQz5ur_6Ye5Z4WchPBRS4t-AHStOVVigkyQ7VhH_3RfR88JHLFWJnnvQkDd25AHE3c5TkYfsWEI_dCCEsWxMNq2ZXgC1VhY4vnaE79ZSyiz1M2mI7ICktGo5TsD8BpSU76G3fqS-4wKFiTgjOhmgRqfbyZC9qlbZWvlrCjUsikx8lTsGSqm-p2cE0REr5JkXd8SSGlxmO7DGA9zAltrZsX7wZQZi1aaiobcws3f1zkGPauA5WzAw6rq0b1EqQuJiaj2sTlomDYISyjMbQkf1cEBHrEUZjtpQP8dCwYJSfxkfJhrpMZikw89XGYCWlVZnN4V22C91SnV1BN2UENwTV7waUFCBeJ12euBI1HU-KufyqfOVB3l25Kj6m00rM8INRgMG57cfPCSLQnWBVWhtaD9SX1_VTpw-rmDQ0odZV1nqRc4zCNuK2hdB7sKeZ3tGs9VVlT4lRmb1JASyOweCt40Cjoh8vNCRh2s1F9C=w2692-h1796-no",
+          "imageUrl": "https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "01-03-2023",
           "comments": []
         },
         {
-          "imageUrl": "https://lh3.googleusercontent.com/-EjmrqK6xFFEpq7finEDe29HeQPCl2PqMJ6RaZS_VT38mm8z0S9GZCRR0Aq4eo9y1QWJ3QHJZttFe77761aG6Es6xCyr9j4lG_u_cfUqevL6BVoWatSplb5oPLmf3Gr4AtozviwhJ27y8D0lYZVfmU2JnRg46PiPF-0L1nFdX_LpIxAj1-78NouFV4HZsVTAxVsZe7Y9eV5DUg0othBQcIvzbK4vbp4fPp8Q9-XRDmpZBlavQly0kFP3l7mTcuyDHlsqMt4ZoVfEfsMu3xUyr71HVSjFBwsgisiN4OywanZghGWkpSxrABfVeStjXYjsB8ftFMs6hfjSHf7BDmkSRrEa2MZdTGZKF7iMMtkg6sLgoU05KJarcAMcfxBaTgdRrI_5VhzhXFfDSINBjaT8g3F5VP27SqeoYSLbdHfBEW9B8FcsK270BRluSuWgrppvoyU0x52J95eW5AMQvBeBBjVH6exuHGyNacCR-CJ9dPisdC8Ko8KJByQtb3f0iCz02iSW8h95eDE6uvsUimPYa2boTjTAimEt1lehjfu7mZ8K0AJSPM7AKlRLWftVXVIe7KDC7EmInijR1kP4aoQ8ltFQ1l2w7FKA1vj8Rges_s2qB75_NTe1YX7vw44KoqC1grFG3dDt85HMVzxEitMUDwXwqwJeTfoNnle6UAsMv1iNmKf4AX4KA-U62yA44XvD3IcZqvyMe-CRU8tgEl40=w2692-h1796-no",
+          "imageUrl": "https://images.unsplash.com/photo-1516635707594-6949bdca3538?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "01-03-2023",
@@ -53,21 +53,21 @@ export const profiles = {
           "comments": []
         },
         {
-          "imageUrl": "https://images.unsplash.com/photo-1570215170761-f056128eda48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+          "imageUrl": "https://images.unsplash.com/photo-1570215170761-f056128eda48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "19-01-2023",
           "comments": []
         },
         {
-          "imageUrl": "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+          "imageUrl": "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "25-11-2022",
           "comments": []
         },
         {
-          "imageUrl": "https://images.unsplash.com/photo-1550305080-4e029753abcf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80",
+          "imageUrl": "https://images.unsplash.com/photo-1550305080-4e029753abcf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "01-01-2018",
@@ -78,7 +78,7 @@ export const profiles = {
     {
       "name": "Ines",
       "userName": "ines.nc",
-      "profilePictureUrl": "https://scontent-yyz1-1.cdninstagram.com/vp/76ea3b079fa7878c69e3efda4d904b21/5CE05208/t51.2885-15/fr/e15/s1080x1080/50163147_125447425173340_667401520449620322_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&se=1",
+      "profilePictureUrl": "https://avatars.githubusercontent.com/u/5904064?v=4",
       "description": "Lorem Ipsum ... ",
       "profileType": "Personal Blog",
       "profileUrl": "https://www.inesakrap.com",
@@ -161,91 +161,56 @@ export const profiles = {
       "following": "320",
       "images": [
         {
-          "imageUrl": "https://instagram.fham3-1.fna.fbcdn.net/vp/930310a0ba0c8476585b8cd6ce6edf2a/5D19094E/t51.2885-15/e35/s320x320/32006586_1877240859006936_3337904743996456960_n.jpg?_nc_ht=instagram.fham3-1.fna.fbcdn.net",
+          "imageUrl": "https://images.unsplash.com/photo-1601987078505-d93c4cc17b5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60",
+          "liked": ['ines.nc', 'tanya', 'tuesday-boris'],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        },
+        {
+          "imageUrl": "https://images.unsplash.com/photo-1533084822039-99306ceb1d57?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60",
+          "liked": ['ines.nc', 'samircro', 'lavilla'],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        },
+        {
+          "imageUrl": "https://images.unsplash.com/photo-1649055984069-e075c1074256?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60",
+          "liked": ['tuesday-boris', 'lavilla'],
+          "description": "Lorem Ipsum, bla bla bla",
+          "timestamp": "01-01-2018",
+          "comments": []
+        },
+        {
+          "imageUrl": "https://images.unsplash.com/photo-1662539090599-b80030d26a32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "01-01-2018",
           "comments": []
         },
         {
-          "imageUrl": "https://instagram.fham3-1.fna.fbcdn.net/vp/2634adbcd4f8f5fa0e6ece6fffcdcef8/5D186F8C/t51.2885-15/e35/s480x480/44224279_280241942607099_2988566304084809558_n.jpg?_nc_ht=instagram.fham3-1.fna.fbcdn.net",
+          "imageUrl": "https://images.unsplash.com/photo-1582845512747-e42001c95638?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "01-01-2018",
           "comments": []
         },
         {
-          "imageUrl": "https://media.cntraveler.com/photos/5aaac20c60ef3f51835c5be5/4:3/w_420,c_limit/Rock-Of-Cashel-GettyImages-165559826.jpg",
+          "imageUrl": "https://images.unsplash.com/photo-1603736087997-5daec6092347?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "01-01-2018",
           "comments": []
         },
         {
-          "imageUrl": "https://exceptionnotfound.net/content/images/2017/05/rubber-duck-art-piece.jpg",
+          "imageUrl": "https://images.unsplash.com/photo-1499334758287-dc8133b315e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "01-01-2018",
           "comments": []
         },
         {
-          "imageUrl": "http://img.fruugo.com/product/6/32/43380326_max.jpg",
-          "liked": [],
-          "description": "Lorem Ipsum, bla bla bla",
-          "timestamp": "01-01-2018",
-          "comments": []
-        },
-        {
-          "imageUrl": "https://www.duckshop.de/images/product_images/info_images/Badneente-3079.JPG",
-          "liked": [],
-          "description": "Lorem Ipsum, bla bla bla",
-          "timestamp": "01-01-2018",
-          "comments": []
-        },
-        {
-          "imageUrl": "https://www.duckshop.de/images/product_images/info_images/badeente-Katze-grau.JPG",
-          "liked": [],
-          "description": "Lorem Ipsum, bla bla bla",
-          "timestamp": "01-01-2018",
-          "comments": []
-        },
-        {
-          "imageUrl": "http://static1.squarespace.com/static/59698a7d29687fd47a2a7c52/5a4a975871c10bd7b353748f/5998fa1703596e31f5d1e99c/1550081233648/zebra.jpg",
-          "liked": [],
-          "description": "Lorem Ipsum, bla bla bla",
-          "timestamp": "01-01-2018",
-          "comments": []
-        },
-        {
-          "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/31GhPm2isCL._SY355_.jpg",
-          "liked": [],
-          "description": "Lorem Ipsum, bla bla bla",
-          "timestamp": "01-01-2018",
-          "comments": []
-        },
-        {
-          "imageUrl": "https://cdn.shopify.com/s/files/1/0604/4801/products/Facial_1.jpeg?v=1514697697",
-          "liked": [],
-          "description": "Lorem Ipsum, bla bla bla",
-          "timestamp": "01-01-2018",
-          "comments": []
-        },
-        {
-          "imageUrl": "https://www.badeendwinkel.nl/4347-large_default/rubber-duck-diver-dr.jpg",
-          "liked": [],
-          "description": "Lorem Ipsum, bla bla bla",
-          "timestamp": "01-01-2018",
-          "comments": []
-        },
-        {
-          "imageUrl": "https://images.esellerpro.com/2243/I/370/81/BATH212-elvis-presley-rubber-duck%20(1).JPG",
-          "liked": [],
-          "description": "Lorem Ipsum, bla bla bla",
-          "timestamp": "01-01-2018",
-          "comments": []
-        },
-        {
-          "imageUrl": "https://www.carrollsirishgifts.com/media/catalog/product/cache/11/small_image/540x540/9df78eab33525d08d6e5fb8d27136e95/c/1/c102146.jpg",
+          "imageUrl": "https://images.unsplash.com/photo-1517191553602-eb5d7d007c33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60",
           "liked": [],
           "description": "Lorem Ipsum, bla bla bla",
           "timestamp": "01-01-2018",
@@ -258,13 +223,6 @@ export const profiles = {
           "timestamp": "01-01-2018",
           "comments": []
         },
-        {
-          "imageUrl": "https://i.ebayimg.com/images/i/142793235394-0-1/s-l1000.jpg",
-          "liked": [],
-          "description": "Lorem Ipsum, bla bla bla",
-          "timestamp": "01-01-2018",
-          "comments": []
-        }
       ]
     }
   ]
